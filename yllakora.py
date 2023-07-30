21649423b11416da759bc1d88f05c273b2829991
Gouranegithub <gouraneabdellah@gmail.com>
2023-07-30 10:49:39 +0200

    changes

--- yllakora.py
+++ yllakora.py
@@ -2,7 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-
+import datetime
 
 
 date= input("please enter a date in the following format MM/DD/YYYY:  ")
@@ -17,8 +17,17 @@ def main(page):
     def get_match_info(championship):
         championship_name= championship.contents[1].find('h2').text.strip()
         
-        all_matches= championship.contents[3].find_all("li",{'class':'item future'})
-       
+                
+        # Convert the user input to a datetime object
+        user_date = datetime.datetime.strptime(date, "%m/%d/%Y").date()
+
+        # Get the current actual date
+        current_date = datetime.date.today()
+        
+        if user_date >= current_date:
+            all_matches= championship.contents[3].find_all("li",{'class':'item future'})
+        else:
+            all_matches= championship.contents[3].find_all("li",{'class':'item finish'})
     
 
         for match in all_matches:           
@@ -26,7 +35,7 @@ def main(page):
             match_result=match_info.find('div',{'class':'MResult'})
             
             match_scor = match_result.find_all('span',{'class':'score'})
-            score= f'{match_scor[0].text.strip()} - {match_scor[1].text.strip()}'
+            score= f'{match_scor[0].text.strip()} |-| {match_scor[1].text.strip()}'
             
             match_time = match_result.find('span',{'class':'time'}).text.strip()
             
@@ -36,14 +45,14 @@ def main(page):
             teamA= match_teams[0].find('p').text.strip()
             teamB= match_teams[1].find('p').text.strip()
             
-            match_details.append({'نوع البطولة': championship_name,'الفريق الاول':teamA,'الفريق الثاني':teamB,'الميعاد':match_time,'النتيجة': score})
+            match_details.append({'نوع البطولة': championship_name,'الفريق الاول':teamA,'الفريق الثاني':teamB,'الميعاد':match_time,'النتيجة': str(score)})
             
     for champ in championship:
         get_match_info(champ)
     
     keys =  match_details[0].keys()   #['نوع البطولة', 'الفريق الاول','الفريق الثاني','الميعاد','النتيجة' ]
    
-    with open('C:\\Users\\ABDELLAH\\Desktop\\selenium\\matches.csv','w', encoding='utf-8-sig', newline='') as output_file:
+    with open('C:\\Users\\ABDELLAH\\Desktop\\yallakora scrapper\\matches.csv','w', encoding='utf-8-sig', newline='') as output_file:
         dict_writer = csv.DictWriter(output_file, keys, delimiter=',' )
         dict_writer.writeheader()
         for mat in match_details:
